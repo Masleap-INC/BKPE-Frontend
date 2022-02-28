78961db3e07eea6337d12fc64bedfd09205282a9
sifat-jaber <sifatjaber@gmail.com>
2022-02-28 15:49:55 +0600

    commit

---
+++ pages/UserRegister.vue
@@ -0,0 +1,25 @@
+<template>
+
+    
+
+    <div>
+
+        <img src="../assets/page-background.png" alt="" class=" object-cover fixed w-full h-full top-0 bottom-0 z-0 ">
+
+        <div>
+            <UserRegisterForm />
+        </div>
+
+    </div>
+
+</template>
+
+
+<script>
+import UserRegisterForm from "../components/UserRegisterPage/UserRegisterForm.vue";
+
+    export default {
+    components: { UserRegisterForm }
+}
+
+</script>
