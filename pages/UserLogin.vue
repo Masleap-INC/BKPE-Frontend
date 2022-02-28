6d8412c5ca6dac13d02c6a81a943959ab08b33cb
sifat-jaber <sifatjaber@gmail.com>
2022-02-28 18:12:20 +0600

    Created User Login Page

---
+++ pages/UserLogin.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div>
+
+        <!-- page-background -->
+
+        <img src="../assets/page-background.png" alt="" class=" object-cover fixed w-full h-full top-0 bottom-0 z-0 ">
+
+        <!-- Form component -->
+
+        <div>
+            <UserLoginForm />
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+import UserLoginForm from "../components/UserLoginPage/UserLoginForm.vue";
+
+    export default {
+    components: { UserLoginForm }
+}
+
+</script>
