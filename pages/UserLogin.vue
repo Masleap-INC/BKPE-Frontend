f779e3e30ff72d1c9078abd914b425449bdad1ad
zidny000 <t.m.zidny@gmail.com>
2022-03-17 02:24:13 +0600

    update dev

--- pages/UserLogin.vue
+++ pages/UserLogin.vue
@@ -9,7 +9,68 @@
         <!-- Form component -->
 
         <div>
-            <UserLoginForm />
+            <section>
+
+                <div class=" bg-black bg-inherit bg-cover h-full w-full py-48 relative">
+
+                    <div class="bg-transparent mx-auto border-4 border-white lg:w-1/3 md:w-2/3 sm:w-5/6 z-10">
+
+                        <!-- Form Heading -->
+
+                        <h2 class="text-center text-4xl text-white font-semibold mt-10">Login</h2>
+
+                        
+                        
+                        <!-- Registration Form -->
+
+                        <div class=" bg-transparent py-5 px-10">
+
+                            <form  @submit.prevent="submit">
+
+
+                                <!-- Email Label & Input -->
+
+                                <div class="my-10">
+
+                                    <label for="email" class="block text-xl text-white mb-5">Email</label>
+
+                                    <input v-model="login.email" type="email" name="email" placeholder="example@example.com" class="block w-full h-10 rounded-md px-5" required>
+
+                                </div>
+
+                                <!-- Password Label & Input -->
+
+                                <div class="my-10">
+
+                                    <label for="password" class="block text-xl text-white mb-5">Password</label>
+
+                                    <input v-model="login.password" type="password" name="password" placeholder="Type your password here" class="block w-full h-10 rounded-md px-5" required>
+
+                                </div>
+
+                                <!-- Register Button -->
+
+                                <div class="my-10 mx-auto w-fit">
+                                    <button type="submit" class="bg-transparent border-2 border-white text-white text-2xl rounded-md px-5 py-2 hover:bg-white hover:text-black hover:ease-in duration-300">Login</button>
+                                </div>
+
+                            </form>
+
+                            <div class="text-white text-xl my-10">
+
+                                <h2 class=" inline-block">Don't have an account?</h2>
+                                <!-- <a href="/UserRegister" class=" inline-block font-bold underline ml-3 hover:-translate-y-2 duration-300">Register</a> -->
+                                <nuxt-link to="/UserRegister" class=" inline-block font-bold underline ml-3 hover:-translate-y-2 duration-300">Register</nuxt-link>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </section>
         </div>
 
     </div>
@@ -17,10 +78,30 @@
 </template>
 
 <script>
-import UserLoginForm from "../components/UserLoginPage/UserLoginForm.vue";
-
+ 
     export default {
-    components: { UserLoginForm }
-}
+        name:"UserLoginPage",
+        // middleware:"authenticated",
+        data() {
+            return {
+            login:{
+                email: '',
+                password: '',
+            }
+            }
+        },
+        methods: {
+            
+        async submit() {
+            try {
+                
+            const response = await this.$auth.loginWith('local', { data: this.login })
+                console.log(response)
+            } catch (err) {
+                console.log(err)
+            }
+            }
+        }
+    }
 
 </script>
