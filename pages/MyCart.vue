5547f5cb59801f45ab4e4e9fc556a98545059412
zidny000 <t.m.zidny@gmail.com>
2022-04-06 16:19:00 +0600

    functionality added to checkout

--- pages/MyCart.vue
+++ pages/MyCart.vue
@@ -31,7 +31,7 @@
 
                 <!-- Items -->
 
-                <div v-for="item in cartItems" :key="item.id" class="py-5">
+                <div v-for="(item,index) in getCart" :key="item.id" class="py-5">
                     <div class="block w-5/6 py-2 border-b-2 border-white">
                         <h2 class>
 
@@ -41,7 +41,7 @@
                                 <!-- Product Image -->
 
                                 <span class="inline-block mr-2 h-14 w-20 top-0">
-                                    <img :src="item.image" alt />
+                                    <img :src="`http://127.0.0.1:8000${item.product.image}`" alt />
                                 </span>
 
                                 <!-- Product Info -->
@@ -50,15 +50,15 @@
 
                                     <!-- Product Name -->
 
-                                    <span class="block mb-1 font-medium">{{item.name}}</span>
+                                    <span class="block mb-1 font-medium">{{item.product.name}}</span>
 
                                     <!-- Product Brand -->
 
-                                    <span class="block mb-1">{{item.brand}}</span>
+                                    <span class="block mb-1">{{item.product.brand}}</span>
 
                                     <!-- Product Price -->
 
-                                    <span class="block font-bold">$ {{item.price}}</span>
+                                    <span class="block font-bold">$ {{item.product.price}}</span>
 
                                 </span>
                             </span>
@@ -68,7 +68,7 @@
                             <span class="inline-block font-bold text-left text-lg w-1/6 ">
                                 <!-- text box -->
  
-                                <input id="qty" type="number" min="1" :max="item.inventory" class="block mr-1 pl-2 text-black w-14" value="1" readonly>
+                                <input id="qty" type="number" min="1" :max="item.inventory" class="block mr-1 pl-2 text-black w-14" :value="item.quantity" readonly>
 
                                 <!-- Qty plus or minus buttons -->
 
@@ -76,11 +76,11 @@
 
                                     <!-- plus button -->
 
-                                    <button type="button" class="inline-block w-6 h-auto bg-transparent border-2 border-white" >+</button>
+                                    <button  type="button" class="inline-block w-6 h-auto bg-transparent border-2 border-white" @click="incCartItmeQty(index)">+</button>
 
                                     <!-- minus button -->
 
-                                    <button class="inline-block w-6 h-auto bg-transparent border-2 border-white">-</button>
+                                    <button class="inline-block w-6 h-auto bg-transparent border-2 border-white" @click="decCartItmeQty(index)">-</button>
                                 </span>
                             </span>       
 
@@ -91,7 +91,7 @@
                                 <!-- Delete Item Button -->
 
                                 <span class="block top-0">
-                                    <button class=" hover:scale-110 duration-300">
+                                    <button class=" hover:scale-110 duration-300" @click="remCartItem(index)">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 stroke-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                         </svg>
@@ -147,92 +147,117 @@
 
 
 <script>
-
+import {mapGetters} from 'vuex'
 export default {
+    
     name: "MyCart",
-
- 
-
     data () {
         return {
             
-            cartItems: [
-                {
-                    id: 1,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    price: 100,
-                    inventory: 5,
-                    image: require('../assets/camaropedia.jpg')
-                },
-                {
-                    id: 2,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    inventory: 5,
-                    price: 200,
-                    image: require('../assets/camaropedia.jpg')
-                },
-                {
-                    id: 3,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    inventory: 5,
-                    price: 300,
-                    image: require('../assets/camaropedia.jpg')
-                },
-                {
-                    id: 4,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    inventory: 5,
-                    price: 400,
-                    image: require('../assets/camaropedia.jpg')
-                },
-                {
-                    id: 5,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    inventory: 5,
-                    price: 500,
-                    image: require('../assets/camaropedia.jpg')
-                },
-                {
-                    id: 6,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    inventory: 5,
-                    price: 600,
-                    image: require('../assets/camaropedia.jpg')
-                },
-                {
-                    id: 7,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    inventory: 5,
-                    price: 700,
-                    image: require('../assets/camaropedia.jpg')
-                },
-                {
-                    id: 8,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    inventory: 5,
-                    price: 800,
-                    image: require('../assets/camaropedia.jpg')
-                },
-                {
-                    id: 9,
-                    name: "Product Name",
-                    brand: "Product Brand",
-                    inventory: 5,
-                    price: 900,
-                    image: require('../assets/camaropedia.jpg')
-                },
+            cartItems: [ 
+                // {
+                //     id: 1,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     price: 100,
+                //     inventory: 5,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
+                // {
+                //     id: 2,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     inventory: 5,
+                //     price: 200,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
+                // {
+                //     id: 3,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     inventory: 5,
+                //     price: 300,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
+                // {
+                //     id: 4,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     inventory: 5,
+                //     price: 400,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
+                // {
+                //     id: 5,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     inventory: 5,
+                //     price: 500,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
+                // {
+                //     id: 6,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     inventory: 5,
+                //     price: 600,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
+                // {
+                //     id: 7,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     inventory: 5,
+                //     price: 700,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
+                // {
+                //     id: 8,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     inventory: 5,
+                //     price: 800,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
+                // {
+                //     id: 9,
+                //     name: "Product Name",
+                //     brand: "Product Brand",
+                //     inventory: 5,
+                //     price: 900,
+                //     image: require('../assets/camaropedia.jpg')
+                // },
                 
             ],
         };
     },
+
+     computed:{
+    
+    ...mapGetters({
+      getCart:'cart/getCart',
+      
+    }),
+    
+  },
+
+    mounted(){
+        
+        this.$store.dispatch("cart/fetchCartLocalstorage");
+        // this.cartItems = [...this.getCart]
+    },
+
+    methods:{
+    incCartItmeQty(index){
+      this.$store.dispatch("cart/incItemQty",index)
+    },
+    decCartItmeQty(index){
+      this.$store.dispatch("cart/decItemQty",index)
+    },
+    remCartItem(index){
+      this.$store.dispatch("cart/remCartItem",index)
+    }
+  }
 
     
 }
