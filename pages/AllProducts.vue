5c49e7d82d8bc587971cf750c5a72668acf95cf2
sifat-jaber <sifatjaber@gmail.com>
2022-05-09 09:57:52 +0600

    initiated necessary component for all product page.

---
+++ pages/AllProducts.vue
@@ -0,0 +1,35 @@
+<template>
+
+    <div  class="bg-[url('~/assets/page-background.png')] bg-fixed h-full w-full pt-20 md:pt-14 sm:pt-14">
+
+        <!-- Nav Bar -->
+
+        <div>
+            <NavBar />
+        </div>
+
+
+        <!-- Categories Section -->
+
+        <div class="mt-20">
+            <CategoriesSection />
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+import NavBar from "../components/Misc/NavBar.vue";
+import CategoriesSection from "../components/Misc/CategoriesSection.vue";
+
+    export default {
+
+        components: {
+            NavBar,
+            CategoriesSection,
+        }
+    }
+
+</script>
