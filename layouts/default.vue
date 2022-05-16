804e29883cb617e83ef05d5993c5ada384af6af3
zidny000 <t.m.zidny@gmail.com>
2022-05-16 19:31:34 +0600

    default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,7 @@
 <template>
-  <div>
+   <div class="pt-20 md:pt-10 sm:pt-10">
       <NavBar />
-      <div>
-           <Nuxt />
-      </div>
-     
+      <Nuxt />
       <Footer />
   </div>
   
