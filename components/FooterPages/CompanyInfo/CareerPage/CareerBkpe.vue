48a7832a634b8ec3fc79aef4bd24abea48587be1
sifat-jaber <sifatjaber@gmail.com>
2022-07-21 17:35:23 +0600

    Created the Career page and required component and linked that to the footer menu

---
+++ components/FooterPages/CompanyInfo/CareerPage/CareerBkpe.vue
@@ -0,0 +1,122 @@
+<template>
+
+    <div class="">
+
+        <!-- Heading -->
+
+        <div class="bg-black bg-opacity-50 text-white w-full py-20 px-10">
+
+            <!-- Main Text -->
+
+            <h1 class="block text-5xl font-bold text-center pt-10 lg:w-2/3 md:w-5/6 sm:w-5/6 mx-auto">CAREERS AT BLACK KNIGHT</h1>
+
+            <!-- Divider Lines -->
+
+            <div class="block h-3 border-y-2 border-y-white my-10 lg:w-2/3 md:w-5/6 sm:w-5/6 mx-auto">
+
+            </div>
+
+            <!-- Sub Text -->
+
+            <p class="block lg:w-2/3 md:w-5/6 sm:w-5/6 mx-auto text-2xl text-center">At the Black Knight associated companies we are always interested in hearing from experienced people who share our passion for performance automobiles. Thus, should you have experience with any of the following, we encourage you to contact us</p>
+
+        </div>
+
+
+        <!-- Jobs Section -->
+
+
+        <div class="lg:grid lg:grid-cols-2 gap-5 mx-10 py-20 text-white">
+
+            <!-- Column 1 -->
+
+            <div class="md:mb-20 sm:mb-20">
+
+                <!-- Job List -->
+
+
+                <div class="lgw-2/3 md:w-5/6 sm:w-5/6 mx-auto">
+
+                    <h2 class="block text-3xl font-semibold mb-10">CURRENT OPENINGS</h2>
+
+                    <ul>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">A/V System Design, Fabrication and Installation.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Automotive Body and Paint.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Automotive Electrical Systems.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Automotive Mechanics.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">ECU/TCU Programming.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Engine Building.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Carbon Fiber/Fiberglass/Metal Fabrication.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Data Entry / Management.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Interior Design, Fabrication and Installation.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Mechanical/Automotive Engineering.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Custom Upholstery.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Customer Service Representation.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Order Fulfillment Specialist.</li>
+
+                        <li class="text-xl border-2 border-white px-5 py-3 mb-3">Office Management and Bookkeeping.</li>
+
+
+                    </ul>
+
+                </div>
+
+                
+            </div>
+
+
+            <!-- Column 2 -->
+
+            <div>
+
+                <div class=" lg:w-2/3 md:w-5/6 sm:w-5/6 mx-auto">
+
+                    <!-- Heading -->
+
+                    <div>
+
+                        <h2 class="block text-3xl font-semibold mb-10">APPLY NOW</h2>
+
+                    </div>
+
+                    <!-- Image -->
+
+                    <div class="block w-full h-auto">
+
+                        <img src="../../../../assets/career.webp" alt="bkpe-career" class="w-full h-auto">
+
+                    </div>
+
+                    <!-- Description -->
+
+                    <div class="w-full my-10">
+
+                        <p class="block text-2xl">Want to be ahead of the curve and have your resume on file with us? Then click on the link below, tell us what contribution you can make to the Black Knight family and attach your resume. If your attitude, education, experience and skill meet any future need, we will gladly contact and arrange an interview.</p>
+
+                        <a href="mailto: careers@bkpe.com" class=" block w-fit mx-auto text-xl font-semibold px-5 py-3 mt-10 border-2 border-white rounded-3xl hover:bg-white hover:text-blue-600 duration-300">EMAIL HUMAN RESOURCES</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </div>
+
+</template>
