3f4d9d0a50fbddb2d0b002b7d76fd1999da0abdd
sifat-jaber <sifatjaber@gmail.com>
2022-09-15 16:05:45 +0600

    Created and designed All sale parts page and all new parts page with the required components

---
+++ components/AllNewPartsPage/AllNewPartsSection.vue
@@ -0,0 +1,304 @@
+<template>
+  <div>
+    <div class="py-10">
+      <!-- Products  -->
+
+      <div class="my-10 w-5/6 mx-auto">
+        <div
+          class="grid grid-flow-row-dense lg:grid-cols-5 lg:gap-y-10 md:grid-cols-3 sm:grid-cols-1 grid-rows-2 gap-5 place-items-center"
+        >
+          <!-- Product 1 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 2 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 3 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 4 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 5 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 6 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 7 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 8 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 9 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+
+          <!-- Product 10 -->
+
+          <div class="bg-white rounded-xl p-2 w-full">
+            <!-- Product Image  -->
+
+            <img
+              src="../../assets/demo-product-image.jpg"
+              alt=""
+              class="w-full h-1/2 object-cover"
+            />
+
+            <!-- Product Title -->
+
+            <h2
+              class="block text-blue-700 text-2xl font-bold my-3 text-ellipsis overflow-hidden w-fit h-8"
+            >
+              Product Name
+            </h2>
+
+            <!-- Product Brand -->
+
+            <h2 class="block text-xl font-bold my-3">BKPP P/N</h2>
+
+            <!-- Product Price -->
+
+            <h2 class="block text-red-700 my-1 text-xl font-bold">$100</h2>
+
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/AllSalePartsPage.vue
@@ -0,0 +1,66 @@
+<template>
+
+    <div  class="bg-[url('~/assets/page-background.png')] bg-fixed bg-cover bg-no-repeat h-full w-full">
+
+        <!-- Top Bar -->
+
+        <div>
+            <TopBar />
+        </div>
+
+        <!-- Back button -->
+
+        <div class="block mt-10">
+            <button class="text-white text-xl ml-5 px-3 py-2 border-2 border-white hover:-translate-x-3 duration-300" @click="goBack">&lt; Go Back</button>
+        </div>
+
+        <!-- Page Heading -->
+
+        <div class="border-b-2 border-white w-5/6 mx-auto pt-10">
+            <h2 class="text-5xl text-white text-center font-semibold py-5 w-fit mx-auto">
+                PARTS ON SALE
+            </h2>
+        </div>
+
+        <!-- Search Bar -->
+
+        <div class="mt-10">
+            <SearchBar />
+        </div>
+
+
+        <!-- All Sale Parts Section -->
+
+        <div>
+            <AllSalePartsSection />
+        </div>
+
+        <!-- Footer -->
+
+        <div>
+            <Footer />
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+
+import AllSalePartsSection from "../components/AllSalePartsPage/AllSalePartsSection.vue";
+import SearchBar from "../components/Misc/SearchBar.vue";
+import Footer from "../components/Misc/Footer.vue";
+import TopBar from "../components/Misc/TopBar.vue";
+
+    export default {
+
+        components: { AllSalePartsSection, SearchBar, Footer, TopBar },
+
+        methods: {
+            goBack() {
+                this.$router.go(-1)
+            }
+        },
+    }
+
+</script>
