d061668c2c3fd5c6aff0b322d30e3dabeed19525
sifat-jaber <sifatjaber@gmail.com>
2022-09-20 16:00:05 +0600

    Created and designed My Account page component and required components such as My Account, MyProfile, MyOrders, MyAddress components

---
+++ components/MyAccountPage/MyOrders.vue
@@ -0,0 +1,87 @@
+<template>
+
+    <div class=" text-white py-10">
+        
+      <!-- Heading -->
+
+      <div
+        class="w-full pb-5 border-b-2 border-white text-2xl font-semibold mb-20"
+      >
+        <h2>MY ORDERS</h2>
+      </div>
+
+      <!-- Order List -->
+
+
+      <div>
+        
+        <!-- Order-1 -->
+
+        <div class="lg:grid md:grid lg:grid-cols-6 md:grid-cols-4 gap-5 p-5 mb-10 border-2 border-white hover:scale-105 duration-200">
+
+          <!-- Order ID & Date -->
+
+          <div class=" col-span-2">
+
+            <!-- ID -->
+
+            <h2 class="block mb-3 font-bold text-2xl">#00098563214</h2>
+
+            <!-- Date -->
+
+            <h2 class="block mb-1 font-semibold text-lg">Placed On</h2>
+
+            <p class=" block lg:mb-0 md:mb-0 sm:mb-5">9-20-22</p>
+
+          </div>
+
+          <!-- Payment Status -->
+
+          <div class="colspan-1 ">
+
+            <!-- Heading -->
+
+            <h2 class="block mb-3 font-semibold text-xl lg:w-fit mx-auto">Payment Status</h2>
+
+            <!-- Value -->
+
+            <p class=" block lg:w-fit mx-auto text-lg sm:mb-5">Paid</p>
+
+          </div>
+
+          <!-- Order Status -->
+
+          <div class=" col-span-1">
+
+            <!-- Heading -->
+
+            <h2 class="block mb-3 font-semibold text-xl lg:w-fit mx-auto">Order Status</h2>
+
+            <!-- Value -->
+
+            <p class=" block lg:w-fit mx-auto text-lg sm:mb-5">Processing</p>
+
+          </div>
+
+
+          <!-- Order Total -->
+
+          <div class=" col-span-2">
+
+            <!-- Heading -->
+
+            <h2 class="block mb-3 font-semibold text-xl lg:w-fit md:w-full sm:w-full lg:text-left md:text-left sm:text-center mx-auto">Order Total</h2>
+
+            <!-- Value -->
+
+            <p class=" block lg:w-fit md:w-full sm:w-full lg:text-left md:text-left sm:text-center mx-auto text-2xl font-bold text-blue-200">$99000</p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+</template>
---
+++ pages/MyAccountPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <div
+    class="bg-[url('~/assets/page-background.png')] bg-fixed bg-cover bg-no-repeat h-full w-full"
+  >
+    <!-- Topbar -->
+
+    <div>
+      <TopBar />
+    </div>
+
+    <!-- Back button -->
+
+    <div class="block mt-10 px-10">
+      <button
+        class="text-white text-xl px-3 py-2 border-2 border-white hover:-translate-x-3 duration-300"
+        @click="goBack"
+      >
+        &lt; Go Back
+      </button>
+    </div>
+
+
+    <!-- Page Heading -->
+
+    <div class="border-b-2 border-white w-5/6 mx-auto pt-5">
+            <h2 class="text-5xl text-white text-center font-semibold py-5 w-fit mx-auto">
+                MY ACCOUNT
+            </h2>
+        </div>
+
+    <!-- My Account Component -->
+
+    <div>
+        <MyAccount />
+    </div>
+
+    <!-- Footer -->
+
+    <div>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import Footer from '../components/Misc/Footer.vue'
+import TopBar from '../components/Misc/TopBar.vue'
+import MyAccount from '../components/MyAccountPage/MyAccount.vue'
+
+export default {
+  components: { Footer, TopBar, MyAccount },
+
+  methods: {
+    goBack() {
+      this.$router.back()
+    },
+  },
+}
+</script>
