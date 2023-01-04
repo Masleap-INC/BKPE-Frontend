4ac32b01135487ddfa9565530163294993e6086b
sifat-jaber <sifatjaber@gmail.com>
2023-01-04 15:04:46 +0600

    Designed Payment Form

--- pages/Payment.vue
+++ pages/Payment.vue
@@ -6,14 +6,24 @@
             <TopBar />
         </div>
         
-        <div v-if="!($fetchState.pending)">
+        <div  v-if="!($fetchState.pending)" class="lg:w-1/3 md:w-2/3 sm:w-5/6 mx-auto my-20">
+
+            <!-- Heading -->
+
+            <h2 class="text-4xl text-white font-bold mb-10">Make Payment</h2>
+
             <stripe-element-payment
             ref="paymentRef"
             :pk="pk"
             :elements-options="elementsOptions"
             :confirm-params="confirmParams"
             />
-            <button @click="pay">Pay Now</button>
+            
+            <div class="w-full flex items-center">
+
+                <button class=" w-fit mx-auto mt-10 px-10 py-2 text-xl text-white hover:bg-transparent border-2 border-white hover:text-black hover:bg-white duration-300" @click="pay">Pay Now</button>
+
+            </div>
         </div>
 
         <!-- Footer -->
@@ -29,11 +39,14 @@ import TopBar from "../components/Misc/TopBar.vue";
 import Footer from "../components/Misc/Footer.vue";
 
 export default {
+
+    name:"PaymentPage",
+
     components: {
         TopBar,
         Footer,
     },
-    name:"PaymentPage",
+    
 
     data() {
        
