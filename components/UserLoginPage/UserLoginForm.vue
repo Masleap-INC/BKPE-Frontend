01598ab818dce21e43ba9f0846d47c933f44f588
sifat-jaber <sifatjaber@gmail.com>
2023-03-03 17:38:13 +0600

    Added Forgot password form to the user login form component and created UserResetPassword page

--- components/UserLoginPage/UserLoginForm.vue
+++ components/UserLoginPage/UserLoginForm.vue
@@ -4,11 +4,11 @@
 
         
 
-        <div class=" bg-black bg-inherit bg-cover h-full w-full py-20 relative">
+        <div class=" bg-black bg-inherit bg-cover h-full w-full py-20 relative min-h-screen">
 
             
 
-            <div class="bg-transparent mx-auto border-4 border-white lg:w-1/3 md:w-2/3 sm:w-5/6 z-10">
+            <div :class="'bg-transparent mx-auto border-4 border-white lg:w-1/3 md:w-2/3 sm:w-5/6 z-10 duration-300 ' + ( forgotPassword ? ' hidden ' : ' block ')">
 
                 <!-- Form Heading -->
 
@@ -16,7 +16,7 @@
 
                 
                 
-                <!-- Registration Form -->
+                <!-- Login Form -->
 
                 <div class=" bg-transparent py-5 px-10">
 
@@ -43,13 +43,21 @@
 
                         </div>
 
-                        <!-- Register Button -->
+                        <!-- Login Button -->
 
                         <div class="my-10 mx-auto w-fit">
                             <button type="submit" class="bg-transparent border-2 border-white text-white text-2xl rounded-md px-5 py-2 hover:bg-white hover:text-black hover:ease-in duration-300">Login</button>
                         </div>
 
                     </form>
+
+                    <!-- Forgot password Button -->
+
+                    <div class="text-white text-xl my-10 w-fit mx-auto rounded-xl">
+                        <button class="block font-bold ml-3 duration-300 px-3 py-2 bg-blue-600 hover:bg-blue-400 rounded-lg" @click="toggleForgotPassword">Forgot Password?</button>
+                    </div>
+
+                    <!-- Register Link -->
 
                     <div class="text-white text-xl my-10">
 
@@ -58,7 +66,52 @@
 
                     </div>
 
-                </div>
+                </div>     
+
+            </div>
+
+            <!-- Forgot Password Form -->
+
+            <div :class="'bg-transparent mx-auto border-4 border-white lg:w-1/3 md:w-2/3 sm:w-5/6 z-10 duration-300 ' + ( forgotPassword ? ' block ' : ' hidden ')">
+
+                <!-- Form Heading -->
+
+                <h2 class="text-center text-xl text-white font-semibold mt-10 px-3">Submit email associated with your account and a password reset link will be sent to your email.</h2>
+
+                
+                
+                <!-- Form -->
+
+                <div class=" bg-transparent py-5 px-10">
+
+                    <form action="">
+
+
+                        <!-- Email Label & Input -->
+
+                        <div class="my-10">
+
+                            <label for="email" class="block text-xl text-white mb-5">Email</label>
+
+                            <input type="email" name="email" placeholder="example@example.com" class="block w-full h-10 rounded-md px-5" required>
+
+                        </div>
+
+                        <!-- Submit Button -->
+
+                        <div class="my-10 mx-auto w-fit">
+                            <button type="submit" class="bg-transparent border-2 border-white text-white text-2xl rounded-md px-5 py-2 hover:bg-white hover:text-black hover:ease-in duration-300">Submit</button>
+                        </div>
+
+                    </form>
+
+                    <!-- Back to Login Button -->
+
+                    <div class="text-white text-xl my-10 w-fit mx-auto rounded-xl">
+                        <button class="block font-bold duration-300 px-3 py-2 bg-blue-600 hover:bg-blue-400 rounded-lg" @click="toggleForgotPassword">Back to Login</button>
+                    </div>
+
+                </div>     
 
             </div>
 
@@ -67,3 +120,31 @@
     </section>
 
 </template>
+
+<script>
+
+export default {
+
+    data() {
+
+        return {
+
+            forgotPassword: false,
+
+        }
+
+    },
+
+    methods: {
+
+        toggleForgotPassword() {
+
+            this.forgotPassword = !this.forgotPassword;
+
+        }
+
+    }
+
+}
+
+</script>
---
+++ pages/UserResetPasswordPage.vue
@@ -0,0 +1,77 @@
+<template>
+
+    <div class="bg-[url('~/assets/page-background.png')] bg-fixed bg-cover bg-no-repeat h-full w-full min-h-screen py-20">
+
+
+        <!-- User Reset Password Form -->
+
+        <div>
+
+            <div class="bg-transparent mx-auto border-4 border-white lg:w-1/3 md:w-2/3 sm:w-5/6 z-10 duration-300 ">
+
+                <!-- Form Heading -->
+
+                <h2 class="text-center text-4xl text-white font-semibold mt-10">Reset Password</h2>
+
+                
+                
+                <!-- Reset Password Form -->
+
+                <div class=" bg-transparent py-5 px-10">
+
+                    <form action="">
+
+
+                        <!-- Email Label & Input -->
+
+                        <div class="my-10">
+
+                            <label class="block text-xl text-white mb-5">Email</label>
+
+                            <h2 class=" text-white text-lg">example@example.com</h2>
+
+                        </div>
+
+                        <!-- Password Label & Input -->
+
+                        <div class="my-10">
+
+                            <label for="password" class="block text-xl text-white mb-5">Password</label>
+
+                            <input type="password" name="password" placeholder="Type new password" class="block w-full h-10 rounded-md px-5" required>
+
+                        </div>
+
+                        <!-- Confirm Password Label & Input -->
+
+                        <div class="my-10">
+
+                            <label for="confirm-password" class="block text-xl text-white mb-5">Confirm Password</label>
+
+                            <input type="password" name="confirm-password" placeholder="Confirm new password" class="block w-full h-10 rounded-md px-5" required>
+
+                        </div>
+
+                        <!-- reset Button -->
+
+                        <div class="my-10 mx-auto w-fit">
+                            <button type="submit" class="bg-transparent border-2 border-white text-white text-2xl rounded-md px-5 py-2 hover:bg-white hover:text-black hover:ease-in duration-300">Reset</button>
+                        </div>
+
+                    </form>
+
+                    <!-- Back to Login Button -->
+
+                    <div class="text-white text-xl my-10 w-fit mx-auto rounded-xl">
+                        <a href="/UserLogin" class="block font-bold duration-300 px-3 py-2 bg-blue-600 hover:bg-blue-400 rounded-lg">Back to Login</a>
+                    </div>
+
+                </div>     
+
+            </div>
+
+        </div>
+
+    </div>
+
+</template>
