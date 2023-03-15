82e7860c293e3ce8babe2eb31f05f69dd22e4a9b
sifat-jaber <sifatjaber@gmail.com>
2023-03-15 17:26:50 +0600

    Created Notification page and required components

---
+++ components/Admin/AdminNotificationsPage/AdminNotificationsPageSection.vue
@@ -0,0 +1,118 @@
+<template>
+  <div>
+    <!-- Heading -->
+
+    <div class="mt-10 mb-5 pb-5 border-b-2 border-b-white">
+      <h2 class="text-4xl font-semibold">Notifications</h2>
+    </div>
+
+    <!-- Notifications -->
+
+    <div class="w-full">
+
+        <ul>
+
+            <li class="w-full mb-5">
+                <h2 class="w-fit p-3 bg-blue-400 bg-opacity-30 rounded-xl lg:w-1/2 md:w-5/6 sm:w-full">
+
+                    <span class="block w-fit ml-auto mb-2">
+
+                        <!-- Date -->
+
+                        <span class="mr-3">3/15/2023</span>
+
+                        <!-- Time -->
+
+                        <span class="mr-3">3:15 PM</span>
+
+                        <!-- View Button -->
+
+                        <span>
+                            <button class="inline-block rounded-xl text-white font-semibold text-md border-2 border-white px-2 hover:bg-white hover:text-blue-600 duration-300">View</button>
+                        </span>
+
+                    </span>
+
+                    <!-- Message -->
+
+                    <span class="block">
+
+                        <span class="mr-5 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</span>
+
+                    </span>
+
+                </h2>
+            </li>
+
+            <li class="w-full mb-5">
+                <h2 class="w-fit p-3 bg-blue-400 bg-opacity-30 rounded-xl lg:w-1/2 md:w-5/6 sm:w-full">
+
+                    <span class="block w-fit ml-auto mb-2">
+
+                        <!-- Date -->
+
+                        <span class="mr-3">3/15/2023</span>
+
+                        <!-- Time -->
+
+                        <span class="mr-3">3:15 PM</span>
+
+                        <!-- View Button -->
+
+                        <span>
+                            <button class="inline-block rounded-xl text-white font-semibold text-md border-2 border-white px-2 hover:bg-white hover:text-blue-600 duration-300">View</button>
+                        </span>
+
+                    </span>
+
+                    <!-- Message -->
+
+                    <span class="block">
+
+                        <span class="mr-5 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</span>
+
+                    </span>
+
+                </h2>
+            </li>
+
+            <li class="w-full mb-5">
+                <h2 class="w-fit p-3 bg-blue-400 bg-opacity-30 rounded-xl lg:w-1/2 md:w-5/6 sm:w-full">
+
+                    <span class="block w-fit ml-auto mb-2">
+
+                        <!-- Date -->
+
+                        <span class="mr-3">3/15/2023</span>
+
+                        <!-- Time -->
+
+                        <span class="mr-3">3:15 PM</span>
+
+                        <!-- View Button -->
+
+                        <span>
+                            <button class="inline-block rounded-xl text-white font-semibold text-md border-2 border-white px-2 hover:bg-white hover:text-blue-600 duration-300">View</button>
+                        </span>
+
+                    </span>
+
+                    <!-- Message -->
+
+                    <span class="block">
+
+                        <span class="mr-5 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</span>
+
+                    </span>
+
+                </h2>
+            </li>
+
+        </ul>
+
+    </div>
+
+ 
+  </div>
+</template>
+
